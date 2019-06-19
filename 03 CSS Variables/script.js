018534b08f0b632b72f25c350a23dc4cab6c62fb
Sasa Radovanovic <salle90ar@gmail.com>
2019-06-19 21:09:58 +0200

    Finished CSS Variables

--- 03 CSS Variables/script.js
+++ 03 CSS Variables/script.js
@@ -1,1 +1,16 @@
-console.log('hi');
+const controlInputs = document.querySelectorAll('.controls > input');
+
+
+
+function handleChange(e) {
+    console.log(this.sizing);
+    const name = this.name;
+    const value = this.value;
+    const sizing = this.dataset.sizing;
+
+    document.documentElement.style.setProperty(`--${name}`, `${value}${sizing}`);
+}
+ 
+controlInputs.forEach(input => input.addEventListener('mousemove', handleChange));
+
+controlInputs.forEach(input => input.addEventListener('change', handleChange));
