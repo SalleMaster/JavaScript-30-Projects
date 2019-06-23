375aa3fe048575f88712d552d89c5244d790c37a
Sasa Radovanovic <salle90ar@gmail.com>
2019-06-23 20:52:42 +0200

    Basic search function finished

--- 06 Type Ahead/script.js
+++ 06 Type Ahead/script.js
@@ -1,12 +1,30 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 
 const cities = [];
 
-const request = async () => {
-    const response = await fetch(endpoint);
-    const json = await response.json();
-    return json;
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => cities.push(...data));
+
+
+
+function handleSearch(e) {
+    const word = new RegExp(this.value, 'gi');
+    const citiesMatch = cities.filter(city => city.city.match(word) || city.state.match(word));
+
+    const html = citiesMatch.map(place => {
+        return `
+        <li>
+        ${place.city}, ${place.state}
+        </li>`
+    }).join('');
+
+    suggestions.innerHTML = html;
+    console.log(html);
 }
 
-let names = cities.map(data => data.city);
+search.addEventListener('keyup', handleSearch);
