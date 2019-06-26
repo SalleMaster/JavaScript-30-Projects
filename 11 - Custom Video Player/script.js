ded818c3b2b0c38ea9b5684825c739a21eda397e
Sasa Radovanovic <salle90ar@gmail.com>
2019-06-26 15:54:08 +0200

    Changed code to bo more cleaner and efficient but still need some work

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -1,114 +1,133 @@
 const player = document.querySelector('.player');
-const video = player.querySelector('.video');
-const controls = player.querySelector('.controls');
-const progress = controls.querySelector('.progress-bar .progress');
-const playBtn = controls.querySelector('.play-pause');
-const sliders = controls.querySelectorAll('input');
-const skippers = controls.querySelectorAll('.skip');
-const progressBar = controls.querySelector('.progress-bar');
+const video = player.querySelector('video');
+const progressBar = player.querySelector('.controls .progress-bar');
+const progress = progressBar.querySelector('.progress');
+const playBtn = player.querySelector('.controls .play-pause');
+const sliders = player.querySelectorAll('.controls [type=range]');
+const skippers = player.querySelectorAll('.controls .skip');
+const defaultPlaybackRate = player.querySelector('.default-playback-rate');
+const time = player.querySelector('.time');
 let mousedown = false;
 
 
+/*** FUNCTIONS ***/
 
-// playbackRate ; volume ; paused: true video.play() video.pause()
-
-
-function handlePlay(e) {
-    const pauseIcon = '<i class="fas fa-pause"></i>'
-    const playIcon = '<i class="fas fa-play"></i>'
-    
-    if(this.classList.contains('play')) {
-        this.classList.remove('play');
-        this.classList.add('pause');
-        this.innerHTML = pauseIcon;
-        video.play();
-    } else {
-        this.classList.remove('pause');
-        this.classList.add('play');
-        this.innerHTML = playIcon;
-        video.pause();
-    }
-}
-
-
-function handleSliders(e) {
-    
-    const value = this.value;
-    const action = this.name;
-    
-    video[action] = value;
-}
-
-
-function handleSkip(e) {
-    const skiping = parseInt(this.dataset.skip);
-    
-
-    const currentTime = video.currentTime + skiping;
-
-    video.currentTime = currentTime;
-}
-
-
-function updateProgress(e) {
-    const currentTime = video.currentTime;
-    const duration = video.duration;
-    const perCent = (100 / duration * currentTime).toFixed(2);
-    progress.style.flexBasis = `${perCent}%`;
-}
-
-
-function changeProgress(e) {
-    const x = e.offsetX;
-    const width = progressBar.clientWidth;
-    const perCent = 100 / width * x;
-    progress.style.flexBasis = `${perCent}%`;
-
-    const currentTime = video.duration / 100 * perCent;
-    video.currentTime = currentTime;
-}
-
-function videoCompleted(e) {
+// Playing Video Function
+function handlePlay() {
     const playIcon = '<i class="fas fa-play"></i>';
-    playBtn.classList.remove('pause');
-    playBtn.classList.add('play');
-    playBtn.innerHTML = playIcon;
-
-}
-
-
-function handleVideoClick() {
-    
-    const pauseIcon = '<i class="fas fa-pause"></i>'
-    const playIcon = '<i class="fas fa-play"></i>'
-    
-    if(playBtn.classList.contains('play')) {
-        playBtn.classList.remove('play');
-        playBtn.classList.add('pause');
-        playBtn.innerHTML = pauseIcon;
+    const pauseIcon = '<i class="fas fa-pause"></i>';
+    if(video.paused) {
         video.play();
+        playBtn.innerHTML = pauseIcon;
     } else {
-        playBtn.classList.remove('pause');
-        playBtn.classList.add('play');
-        playBtn.innerHTML = playIcon;
         video.pause();
+        playBtn.innerHTML = playIcon;
     }
+
 }
 
-playBtn.addEventListener('click', handlePlay);
-sliders.forEach(slider => slider.addEventListener('click', handleSliders));
-sliders.forEach(slider => slider.addEventListener('mousemove', handleSliders));
-skippers.forEach(skip => skip.addEventListener('click', handleSkip));
-video.addEventListener('timeupdate', updateProgress);
-video.addEventListener('ended', videoCompleted);
-video.addEventListener('click', handleVideoClick);
-progressBar.addEventListener('click', changeProgress);
-progressBar.addEventListener('mousedown',() => mousedown = true);
-progressBar.addEventListener('mouseup',() => mousedown = false);
-progressBar.addEventListener('mousemove',(e) => {
+
+// Slider Function
+function handleSliders() {
+    const name = this.name;
+    const value = this.value;
+    video[name] = value;
+}
+
+
+// Skip Function
+function handleSkip() {
+    const currentTime = video.currentTime;
+    const updatedTime = currentTime + parseInt(this.dataset.skip);
+    
+    video.currentTime = updatedTime;
+}
+
+// Handle Progress
+function handleProgress() {
+    const perCent = (100 / video.duration * video.currentTime).toFixed(2);
+    progress.style.flexBasis = `${perCent}%`;
+    time.innerHTML = video.currentTime;
+}
+
+
+// Update Progress
+function updateProgress(e) {
     if(!mousedown) return;
-    changeProgress(e);
-});
+    const x = e.layerX;
+    const playPercent = parseFloat((100 / progressBar.offsetWidth * x).toFixed(2));
+    const updatedTime = parseFloat((video.duration / 100 * playPercent).toFixed(2));
+    
+    video.currentTime = updatedTime;
+}
 
 
+// Default playbackRate function
+function defaultRate() {
+    video.playbackRate = 1;
+    sliders.forEach(slider => {
+        if(slider.name === 'playbackRate') {
+            slider.value = 1;
+        }
+    });
+}
+
+
+// Function for converting seconds to HH:MM:SS format
+
+function formatSeconds(sec) {
+    let hour = 0;
+    let minute = Math.floor(sec / 60);
+    let second = Math.floor(sec % 60);
+    if(minute > 60) {
+        let hour = Math.floor(minute / 60);
+        minute = Math.floor(minute % 60)
+    }
+
+    let time;
+
+    if(hour === 0) {
+        time = `${minute}:${second < 10 ? '0': ''}${second}`
+    } else {
+        time = `${hour}:${minute < 10 ? '0': ''}${minute}:${second < 10 ? '0': ''}${second}`;
+    }
+    
+    
+
+    return time;
+}
+
+
+
+
+/*** LISTENERS ***/
+
+// Mousedown checking
+progressBar.addEventListener('mousedown', () => mousedown = true);
+progressBar.addEventListener('mouseup', () => mousedown = false);
+
+// Playback listeners
+video.addEventListener('click', handlePlay);
+playBtn.addEventListener('click', handlePlay);
+
+// Slider listeners
+sliders.forEach(slider => slider.addEventListener('mousemove', handleSliders));
+sliders.forEach(slider => slider.addEventListener('click', handleSliders));
+
+
+// Skipper Listeners
+skippers.forEach(skiper => skiper.addEventListener('click', handleSkip));
+
+
+// Progress Listener
+video.addEventListener('timeupdate', handleProgress);
+
+
+// Progress Bar listener for drag
+progressBar.addEventListener('mousemove', updateProgress);
+progressBar.addEventListener('mousedown', updateProgress);
+
+
+// Default playbackRate listener
+defaultPlaybackRate.addEventListener('click', defaultRate);
 
