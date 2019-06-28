6f35370b952cce31e2d3647ecefc43f17b13d0ac
Sasa Radovanovic <salle90ar@gmail.com>
2019-06-28 20:45:33 +0200

    A little bit diffrent implementation than Wes Bos

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -1,27 +1,29 @@
 const header = document.querySelector('h2');
+const walk = 500;
 
 
 function handleShadow(e) {
+    // Mouse coordinates
     const mouseX = e.clientX;
     const mouseY = e.clientY;
+
+    // Header middle coordinates
     const headerX = header.offsetLeft + header.offsetWidth / 2;
     const headerY = header.offsetTop + header.offsetHeight / 2;
-    let shadowX = headerX - mouseX;
-    let shadowY = headerY - mouseY;
 
-    if(shadowX > 150) {
-        shadowX = 150;
-    }
+    // Coords for shadow
+    let shadowX = (headerX - mouseX);
+    let shadowY = (headerY - mouseY);
     
-    if(shadowX < -150) {
-        shadowX = -150;
-    }
 
-
-    if(shadowY > 150) {
-        shadowY = 150;
-    }
-    header.style.textShadow = `${shadowX}px ${shadowY}px 0 black`;
-    console.log({shadowX, shadowY});
+    
+    header.style.textShadow =  `
+    ${shadowX}px ${shadowY}px 0 lightgreen,
+    ${shadowX * -1}px ${shadowY}px 0 aqua,
+    ${shadowX}px ${shadowY * -1}px 0 pink
+  `;
+    
 }
+
+
 window.addEventListener('mousemove', handleShadow);
