ed7540dfcedf2897befeafd1b4ac25b9bd8b00f4
Sasa Radovanovic <salle90ar@gmail.com>
2019-07-06 16:09:20 +0200

    drag working as intended

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -1,17 +1,37 @@
 const dragContainer = document.querySelector('.drag-container');
+let startX;
+let lastScroll = 0;
 
 // Check if dragging
 let dragging = false;
 dragContainer.addEventListener('mouseleave',() => {
-    console.log('mouseleave');
     dragging = false;
+    lastScroll = dragContainer.scrollLeft;
+    dragContainer.classList.remove('active');
 });
-dragContainer.addEventListener('mousedown',() => dragging = true);
-dragContainer.addEventListener('mouseup',() => dragging = false);
+dragContainer.addEventListener('mousedown',(e) => {
+    dragging = true;
+    startX = e.clientX;
+    dragContainer.classList.add('active');
+});
+dragContainer.addEventListener('mouseup',() => {
+    dragging = false;
+    lastScroll = dragContainer.scrollLeft;
+    dragContainer.classList.remove('active');
+});
 
 function drag(e) {
-    console.log(dragging);
     if (!dragging) return;
+    const walk = e.clientX - startX;
+    dragContainer.scrollLeft = lastScroll - walk;
 }
 
 dragContainer.addEventListener('mousemove', drag);
+
+
+// Prevent selecting text on whole body
+const body = document.querySelector('body');
+
+body.addEventListener('mousedown', (e) => {
+    e.preventDefault(); // Prevent from selecting text witch will break dragging
+})
